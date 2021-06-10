65e9356ea3afb5ec598e1e4baf9747479ab75a9c
George Almousa <george.almousa@progressoft.com>
2021-06-10 07:07:30 +0000

    Add new file

---
+++ script.sh
@@ -0,0 +1,3 @@
+date=`git log -1 > gitlog.txt && d=$(date +%Y-%m-%d)  &&  echo -n $d | awk -F "-" '{print $1 $2 $3}'` # for finding the date
+commit8=`sed -n 1p gitlog.txt | awk -F " " '{print $2}' | cut -c1-8` && rm gitlog.txt # for getting 8 digit of the commit
+echo $date"-"$commit8
